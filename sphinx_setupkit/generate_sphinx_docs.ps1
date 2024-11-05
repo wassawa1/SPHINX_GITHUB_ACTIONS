826d36ebeab7966692c3d560f0735fd260a045c3
wassawa1 <wwassa737@gmail.com>
2024-11-05 13:05:38 +0900

    revise workflow3

--- sphinx_setupkit/generate_sphinx_docs.ps1
+++ sphinx_setupkit/generate_sphinx_docs.ps1
@@ -2,9 +2,9 @@
 $json = (Get-Content "sphinx_settings.json" | ConvertFrom-Json)
 
 # Check if the ./docs directory exists and delete it if it does
-if (Test-Path "./docs") {
-    Remove-Item "./docs" -Recurse -Force
+if (Test-Path "../docs") {
+    Remove-Item "../docs" -Recurse -Force
 }
 
 # execute sphinx-apidoc using template files (customize ./docs/conf.py)
-sphinx-apidoc -F -o "./docs" -A $json.author -H $json.project -V $json.version -t ./templates $json.source_dir 
+sphinx-apidoc -F -o "../docs" -A $json.author -H $json.project -V $json.version -t ./templates $json.source_dir 
